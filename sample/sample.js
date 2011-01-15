807ce014bab7259aaf5a6e595b76c74b1e6f75e0
Guy Royse <guy@guyroyse.com>
2011-01-15 05:35:32 -0500

    Adding file and 404 support

--- sample/sample.js
+++ sample/sample.js
@@ -2,7 +2,12 @@ var scurvy = require('../src/scurvy');
 
 scurvy.get('/hello', function(request, response) {
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end('Hello World\n');
+	response.end('Hello World!\n');
+});
+
+scurvy.get('/hello/es', function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.end('Hola Mundo!\n');
 });
 
 scurvy.createServer().listen(8124);
--- src/scurvy.js
+++ src/scurvy.js
@@ -1,17 +1,47 @@
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
 var routes = [];
 
+var findMatchingRoutes = function(request, response) {
+	for (var i = 0; i < routes.length; i++) {
+		var route = routes[i];
+		if (request.url === route.matcher) {
+			route.func(request, response);
+			return true;
+		}
+	}
+	return false;
+};
+
+var findMatchingFiles = function(request, response) {
+	var path = url.parse(request.url).pathname;
+	fs.readFile('public' + path, function (err, data) {
+		if (err) { 
+			write404(request, response);
+		} else {
+			response.writeHead(200);
+			response.end(data);
+		} 
+	});
+};
+
+
+var write404 = function(request, response) {
+	fs.readFile('404.html', function (err, data) { 
+		response.writeHead(404, {'Content-Type': 'text/html'});
+		response.end(data);
+	});
+	console.log(' - Not Found!');
+};
+
 exports.createServer = function () {
+	console.log("Arrrr! There be a scurvy server startin'."); 
 	return http.createServer(function(request, response) {
-		routes.forEach(function(route) {
-			if (request.url === route.matcher) {
-				route.func(request, response);
-				return;
-			}
-		});
-		response.writeHead(404);
-		response.end('Not found!');
+		console.log(request.method + ' ' + request.url); 
+		if (findMatchingRoutes(request, response)) return;
+		findMatchingFiles(request, response);
 	});
 };
 
